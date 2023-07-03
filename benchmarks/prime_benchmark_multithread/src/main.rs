9366f0fac6878703a4d2b846063c85d0fccbb089
Ediblelnk <peters1731@gmail.com>
2023-07-03 01:01:32 -0400

    did the thing for multithreaded

--- benchmarks/prime_benchmark_multithread/src/main.rs
+++ benchmarks/prime_benchmark_multithread/src/main.rs
@@ -1,20 +1,51 @@
-use std::{sync::mpsc, thread, time::Instant};
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+    time::Instant,
+};
 
-const UPPER_BOUND: usize = 10_000;
-const LOWER_BOUND: usize = 2;
+const UPPER_BOUND: usize = 1_000_000;
+const LOWER_BOUND: usize = 0;
 
-const NUM_THREADS: usize = 2;
+const NUM_THREADS: usize = 10;
+
+struct Range {
+    lower: usize,
+    upper: usize,
+}
 
 fn main() {
-    let (tx, rx) = mpsc::channel::<usize>();
+    assert!((UPPER_BOUND - LOWER_BOUND) % NUM_THREADS == 0);
+
+    let composites: Arc<Mutex<usize>> = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
     let time = Instant::now();
 
-    let primes = UPPER_BOUND
-        - find_composites(&Range {
-            lower: LOWER_BOUND,
-            upper: UPPER_BOUND,
+    for i in 0..NUM_THREADS {
+        let (thread_lower, thread_upper) =
+            find_thread_range(&i, &LOWER_BOUND, &(UPPER_BOUND / NUM_THREADS));
+        println!("{} {}", thread_lower, thread_upper);
+
+        let composites = Arc::clone(&composites);
+        let handle = thread::spawn(move || {
+            let mut num = composites.lock().unwrap();
+
+            let amount = find_composites(&Range {
+                lower: thread_lower,
+                upper: thread_upper,
+            });
+
+            *num += amount;
         });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    let primes: usize = UPPER_BOUND - *composites.lock().unwrap();
 
     let duration = time.elapsed();
 
@@ -25,22 +56,17 @@ fn main() {
     println!("Calculations took {:?}", duration);
 }
 
-struct Range {
-    lower: usize,
-    upper: usize,
-}
-
 fn find_composites(range: &Range) -> usize {
     let mut value = range.lower;
-    let mut composites = 1;
+    let mut composites = 0;
 
     let mut test: usize;
 
     while value < range.upper {
         value += 1;
-        let value_bound = f64::sqrt(value as f64) + 1.0;
-
         test = 2;
+
+        let value_bound = f64::sqrt(value as f64) + 1.0;
 
         while test < value_bound as usize {
             if value % test == 0 {
@@ -50,5 +76,14 @@ fn find_composites(range: &Range) -> usize {
             test += 1;
         }
     }
+
+    if range.lower == 0 {
+        composites += 1;
+    }
+
     composites
+}
+
+fn find_thread_range(i: &usize, start: &usize, step: &usize) -> (usize, usize) {
+    (start + i * step, start + (i + 1) * step) // returns lower, upper
 }
