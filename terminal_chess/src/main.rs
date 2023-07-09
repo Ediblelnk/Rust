62bab85773da2ac42ba6fc803af701b617367d8b
Ediblelnk <peters1731@gmail.com>
2023-07-09 16:05:36 -0400

    printing

--- terminal_chess/src/main.rs
+++ terminal_chess/src/main.rs
@@ -1,5 +1,20 @@
 use terminal_chess::*;
 
 fn main() {
-    print_piece(BLACK, BLACK, KING)
+    // print_piece(BLACK, K);
+
+    let chess = Chess::new([
+        [r, n, b, q, k, b, n, r],
+        [p, p, p, p, p, p, p, p],
+        [E, E, E, E, E, E, E, E],
+        [E, E, E, E, E, E, E, E],
+        [E, E, E, E, P, E, E, E],
+        [E, E, E, E, E, E, E, E],
+        [P, P, P, P, E, P, P, P],
+        [R, N, B, Q, K, B, N, R],
+    ]);
+
+    chess.print_board();
+
+    chess.move_piece("a2a4");
 }
