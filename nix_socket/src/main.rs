d4b34f8747ee4f6c5ced37bd1d9612b57470c2cd
Ediblelnk <peters1731@gmail.com>
2023-10-26 08:53:33 -0400

    yup

--- nix_socket/src/main.rs
+++ nix_socket/src/main.rs
@@ -1,5 +1,8 @@
-use nix::sys::socket;
+use nix::sys::socket::*;
 
 fn main() {
-    
+    match socket(AddressFamily::Inet, SockType::Stream, SockFlag::empty(), None) {
+        Ok(_) => println!("It worked!"),
+        Err(why) => println!("DID NOT WORK: {}", why),
+    }
 }
